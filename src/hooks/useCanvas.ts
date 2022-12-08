0e297fdbb850f986d25a80edd63a95f8f2a4e66e
NataliaZhydeikina <nata.iiinx88@gmail.com>
2022-12-08 11:18:01 +0200

    useResizeCanvas & useAnimateCanvas

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Sketch from "../models/Sketch";
+import useResizeCanvas from "./useResizeCanvas";
+import useAnimateCanvas from "./useAnimateCanvas";
 
 export default function useCanvas(width = innerWidth, height = innerHeight) {
-	const sketch = new Sketch({ width, height });
+	const sketch = useAnimateCanvas(useResizeCanvas(new Sketch({width, height})));
 
 	useEffect(() => {
 		sketch.init();
-	});
+	}, []);
 
 	return sketch.getRef();
 }
--- src/models/Sketch/Sketch.ts
+++ src/models/Sketch/Sketch.ts
@@ -7,6 +7,7 @@ import {
 	PerspectiveCamera,
 	WebGLRenderer,
 } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export default class Sketch {
 	private time: number;
@@ -21,12 +22,12 @@ export default class Sketch {
 	private geometry!: BoxGeometry;
 	private material!: MeshNormalMaterial;
 	private mesh!: Mesh;
+	private controls!: OrbitControls;
 
 	constructor(options: { width: number; height: number }) {
 		this.ref = useRef<HTMLCanvasElement>(null);
 		this.time = 0;
 		this.scene = new Scene();
-
 		this.width = options.width;
 		this.height = options.height;
 
@@ -44,9 +45,14 @@ export default class Sketch {
 		});
 		this.renderer.setSize(this.width, this.height);
 		this.renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+		this.controls.enableDamping = true;
 
 		this.addObjects();
 		this.render();
+
+		this.resize = this.resize.bind(this);
+		this.render = this.render.bind(this);
 	}
 
 	render() {
@@ -54,12 +60,20 @@ export default class Sketch {
 
 		this.mesh.rotation.x = this.time / 2000;
 		this.mesh.rotation.y = this.time / 2000;
+		this.controls.update();
 		this.renderer.render(this.scene, this.camera);
-
-		//requestAnimationFrame(this.render);
 	}
 
-	resize() {}
+	resize(width: number, height: number) {
+		this.width = width;
+		this.height = height;
+		this.camera.aspect = this.width / this.height;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(this.width, this.height);
+		this.renderer.setPixelRatio(Math.min(devicePixelRatio, 2));	
+		
+		this.render();
+	}
 
 	addObjects() {
 		this.geometry = new BoxGeometry(0.2, 0.2, 0.2);
